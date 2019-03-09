c3b011fbb7ba01b9d89609b2576bff721f03a278
nitu <nituk650@gmail.com>
2019-03-09 22:02:13 +0530

    done

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,135 +1,52 @@
 
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Button from '@material-ui/core/Button';
-
-
-
-
-
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import galleryData from './galleryData';
-
-
-
-
-
-
-
-// const styles = {
-
-  const styles = theme => ({
-
-
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -18,
-    marginRight: 10
-  },
-
-
-
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-});
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-function Gallery(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Typography variant="h6" color="inherit">
-            Welcome to my Gallery
-          </Typography>
-      </AppBar>
-        
-
-          <div>
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span" className={classes.button}>
-          Upload
-        </Button>
-      </label>
-      </div>
-
-
-
-      <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">December</ListSubheader>
-        </GridListTile>
-        {galleryData.map(gallery => (
-          <GridListTile key={gallery.img}>
-            <img src={gallery.img} alt={gallery.title} />
-            <GridListTileBar
-              title={gallery.title}
-              subtitle={<span>by: {gallery.author}</span>}
-              actionIcon={
-                <IconButton className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-
-      
-
+import React, { Component } from 'react';
+class Gallery extends Component {
+  render() {
+    return (
+      <div>
+  <div class="alert alert-success" role="alert">
+  Welcome to Gallery App
 </div>
 
-      
+  <button type="upload" className="btn btn-primary">Upload></button>
 
 
-  );
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWUPmV612kd57STWFH2qtAb-_CVzHtzH9h4Mm5V6iTVns2mejv" />
+    </div>
+
+    <div class="col-sm">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYMbujuXacB5aEV_TbUVS_DOdYEWgWE10RmfFF9O5uYMGqODzbmw" />
+    </div>
+
+    <div class="col-sm">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiUoYtLMRR384z5-dVrjO9JDSKSP2zMfVDjIOV2Om5ox7rEOtj" alt="cat" class="cat" />
+    </div>
+
+    <div class="col-sm">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiUoYtLMRR384z5-dVrjO9JDSKSP2zMfVDjIOV2Om5ox7rEOtj" alt="cat" class="cat" />
+    </div>
+
+
+
+  </div>
+</div>
+</div>
+
+    );
+  }
 }
 
-Gallery.propTypes = {
-  classes: PropTypes.object.isRequired
-};
+export default Gallery;
 
-export default withStyles(styles)(Gallery);
+
+
+
+
+
+
+
+
